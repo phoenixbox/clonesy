c00f93a5465f1b7a253dd078c373f4f491479ef5
Raphael Weiner <raphael.weiner@gmail.com>
2013-04-17 20:44:07 -0600

    Revert seed to original beauty

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,50 +43,43 @@ def seed_users(count)
 end
 
 # THE USUAL SUSPECS / UBERS
-# user1 = User.create(full_name: "Jeff", email: "demoXX+jeff@jumpstartlab.com", password: "password", display_name: "j3")
-# user1.uber_up
-# user2 = User.create(full_name: "Steve Klabnik", email: "demoXX+steve@jumpstartlab.com", password: "password", display_name: "SkrilleX")
-# user2.uber_up
+user1 = User.create(full_name: "Jeff", email: "demoXX+jeff@jumpstartlab.com", password: "password", display_name: "j3")
+user1.uber_up
+user2 = User.create(full_name: "Steve Klabnik", email: "demoXX+steve@jumpstartlab.com", password: "password", display_name: "SkrilleX")
+user2.uber_up
 
 # CREATE STORES
-# store1 = Store.create!(name: "Brad's Bean Bags", path: "bean-bags-galore", description: "the bestest bean bags")
-# store2 = Store.create!(name: "Jorge's Tortiallas", path: "tortilla-palace", description: "get yo tortilla on!")
-# store3 = Store.create!(name: "Paul's PB&Js", path: "pbj", description: "peanut-butter-jelly-time")
-# store4 = Store.create!(name: "Raphael's Rickshaws", path: "rick-roll-rickshaws", description: "rick roll up in this!")
-# store5 = Store.create!(name: "George's Cool Market", path: "cool-market", description: "it's market time")
-# store6 = Store.create!(name: "Burger Master", path: "burger-master", description: "the king of burgers")
-# store7 = Store.create!(name: "Ice Cream Galore", path: "ice-cream-galore", description: "we loves us some ice cream")
-# store8 = Store.create!(name: "Wendy's Magical Den", path: "wendys", description: "The queen of magical Dens")
-# store9 = Store.create!(name: "McGonal's Goo", path: "gooey-goo", description: "sticky icky icky")
-# store10 = Store.create!(name: "Mike's Soft Lemonade", path: "soft-lemonade", description: "there can be only one")
+store1 = Store.create!(name: "Brad's Bean Bags", path: "bean-bags-galore", description: "the bestest bean bags")
+store2 = Store.create!(name: "Jorge's Tortiallas", path: "tortilla-palace", description: "get yo tortilla on!")
+store3 = Store.create!(name: "Paul's PB&Js", path: "pbj", description: "peanut-butter-jelly-time")
+store4 = Store.create!(name: "Raphael's Rickshaws", path: "rick-roll-rickshaws", description: "rick roll up in this!")
+store5 = Store.create!(name: "George's Cool Market", path: "cool-market", description: "it's market time")
+store6 = Store.create!(name: "Burger Master", path: "burger-master", description: "the king of burgers")
+store7 = Store.create!(name: "Ice Cream Galore", path: "ice-cream-galore", description: "we loves us some ice cream")
+store8 = Store.create!(name: "Wendy's Magical Den", path: "wendys", description: "The queen of magical Dens")
+store9 = Store.create!(name: "McGonal's Goo", path: "gooey-goo", description: "sticky icky icky")
+store10 = Store.create!(name: "Mike's Soft Lemonade", path: "soft-lemonade", description: "there can be only one")
 
-stores = [Store.find(1), Store.find(2), Store.find(3), Store.find(4), Store.find(5), Store.find(6), Store.find(7), Store.find(8), Store.find(9), Store.find(10)]
-# store8 = Store.find(8)
-# store9 = Store.find(9)
-# store10 = Store.find(10)
+stores = [store1, store2, store3, store4, store5, store6, store7, store8, store9, store10]
 
 # SET STORE STATUS
-# store1.update_attributes({status: 'online'}, as: :uber)
-# store2.update_attributes({status: 'online'}, as: :uber)
-# store3.update_attributes({status: 'online'}, as: :uber)
-# store4.update_attributes({status: 'declined'}, as: :uber)
-# store5.update_attributes({status: 'declined'}, as: :uber)
+store1.update_attributes({status: 'online'}, as: :uber)
+store2.update_attributes({status: 'online'}, as: :uber)
+store3.update_attributes({status: 'online'}, as: :uber)
+store4.update_attributes({status: 'declined'}, as: :uber)
+store5.update_attributes({status: 'declined'}, as: :uber)
 # store 6-8 are pending by default
-# store9.update_attributes({status: 'offline'}, as: :uber)
-# store10.update_attributes({status: 'offline'}, as: :uber)
+store9.update_attributes({status: 'offline'}, as: :uber)
+store10.update_attributes({status: 'offline'}, as: :uber)
 
 # CREATE CATEGORIES
-# stores.each { |store| seed_categories(store, 10) }
+stores.each { |store| seed_categories(store, 10) }
 
 # CREATE PRODUCTS
-# stores.each { |store| seed_products(store, 10_000) }
-
-# seed_products(store8, 6_000)
-# seed_products(store9, 10_000)
-# seed_products(store10, 10_000)
+stores.each { |store| seed_products(store, 10_000) }
 
 # CREATE USERS
-# seed_users(10_000)
+seed_users(10_000)
 
 # CREATE ROLES
 stores.each do |store|
