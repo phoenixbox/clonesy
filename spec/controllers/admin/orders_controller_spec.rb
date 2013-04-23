baae9f35a9fa0e165f05b274448b6bcfe69648cd
Raphael Weiner <raphael.weiner@gmail.com>
2013-04-22 22:45:05 -0600

    admin/orders_controller_spec.rb passing

--- spec/controllers/admin/orders_controller_spec.rb
+++ spec/controllers/admin/orders_controller_spec.rb
@@ -1,43 +1,41 @@
 require 'spec_helper'
 
 describe Admin::OrdersController do
-  it "index action should render index template" do
-    user = FactoryGirl.create(:uber)
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @store = FactoryGirl.create(:store)
     controller.stub(:require_admin => true)
-    controller.stub(:current_user => user)
+    controller.stub(:current_user => @user)
+    controller.stub(:current_store => @store)
+  end
+
+  it "index action should render index template" do
 
     get :index
     response.should render_template(:index)
   end
 
   it "index action should return a collection of orders" do
-    user = FactoryGirl.create(:uber)
-    orders = [FactoryGirl.create(:order, user: user)]
-    controller.stub(:require_admin => true)
-    controller.stub(:current_user => user)
+    orders = [ FactoryGirl.create(:order, user: @user, store: @store) ]
 
     get :index
     expect(assigns(:orders)).to match_array orders
   end
 
   it "show action should return an individual order" do
-    user = FactoryGirl.create(:uber)
-    order = FactoryGirl.create(:order, user: user)
-    controller.stub(:require_admin => true)
-    controller.stub(:current_user => user)
+    order = FactoryGirl.create(:order, user: @user, store: @store)
+
     get :show, id: order.id
     expect(assigns(:order)).to eq order
   end
 
   describe 'update' do
     it 'works correctly' do
-      user = FactoryGirl.create(:uber)
-      order = FactoryGirl.create(:order, user_id: user.id)
-      controller.stub(:require_admin => true)
-      controller.stub(:current_user => user)
+      order = FactoryGirl.create(:order, user: @user, store: @store)
       request.env["HTTP_REFERER"] = '/'
-      post :update, id: order.id, update_status: 'pending'
-      expect(order.status).to eq 'pending'
+      put :update, id: order.id, update_status: true
+      order.reload
+      expect(order.status).to eq 'cancelled'
     end
   end
 end
