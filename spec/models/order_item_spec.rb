c7fbba236392793a0d70807403e784b07e4a03fa
Geoff Schorkopf <gschorkopf@gmail.com>
2013-04-23 12:14:25 -0600

    Fixed order item tests

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,8 +1,13 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe OrderItem do
+  let!(:user){FactoryGirl.create(:user)}
+  let!(:store){FactoryGirl.create(:store)}
+  let!(:product){FactoryGirl.create(:product, store: store)}
+  let!(:order){FactoryGirl.create(:order, store: store, user: user)}
+
   it 'has a valid factory' do
-    expect(FactoryGirl.build(:order_item)).to be_valid
+    expect(FactoryGirl.build(:order_item, order: order, product: product)).to be_valid
   end
 
   it 'is invalid without a product' do
@@ -14,10 +19,14 @@ describe OrderItem do
   end
 
   it 'is invalid without a valid unit price' do
-    expect(FactoryGirl.build(:order_item, unit_price: nil)).to_not be_valid
-    expect(FactoryGirl.build(:order_item, unit_price: 0)).to_not be_valid
-    expect(FactoryGirl.build(:order_item, unit_price: 1.245)).to_not be_valid
-    expect(FactoryGirl.build(:order_item, unit_price: 1.24)).to be_valid
+    expect(FactoryGirl.build(:order_item, unit_price: nil,
+            order: order, product: product)).to_not be_valid
+    expect(FactoryGirl.build(:order_item, unit_price: 0,
+            order: order, product: product)).to_not be_valid
+    expect(FactoryGirl.build(:order_item, unit_price: 1.245,
+            order: order, product: product)).to_not be_valid
+    expect(FactoryGirl.build(:order_item, unit_price: 1.24,
+            order: order, product: product)).to be_valid
   end
 
   it 'is invalid without an integer quantity above zero' do
