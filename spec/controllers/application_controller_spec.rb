91d92c2e517bd7cdfb40f0bbc8d9e543c2630bf2
Raphael Weiner <raphael.weiner@gmail.com>
2013-04-23 13:25:19 -0600

    application_controller_spec done

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -29,6 +29,40 @@ describe ApplicationController do
     end
   end
 
+  describe 'require_uber' do
+    it 'when false raises not authenticated' do
+      mock_user = mock('Current User', uber?: false)
+      controller.stub(:current_user).and_return(mock_user)
+      controller.should_receive(:not_authenticated)
+      controller.require_uber
+    end
+
+    it 'passes when uber' do
+      mock_user = mock('Current User', uber?: true)
+      controller.stub(:current_user).and_return(mock_user)
+      controller.should_not_receive(:not_authenticated)
+      controller.require_uber
+    end
+  end
+
+  describe 'not_authenticated' do
+    it 'redirects user' do
+      controller.should_receive(:redirect_to)
+      controller.not_authenticated
+    end
+  end
+
+  describe 'require_current_store' do
+    it 'raises RoutingError if no store' do
+      expect { controller.require_current_store }.to raise_error
+    end
+
+    it 'passes when current store exists' do
+      controller.stub(:current_store => true)
+      expect { controller.require_current_store }.to_not raise_error
+    end
+  end
+
   describe 'flag' do
     it 'returns the correct flag' do
       session[:i18n] = 'en'
