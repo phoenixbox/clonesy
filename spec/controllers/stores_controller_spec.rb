a283731d26eb8bce304018a738d51672850d0c83
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-04-23 10:13:15 -0600

    added products controller

---
+++ spec/controllers/stores_controller_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe StoresController do
+
+  let!(:store) {FactoryGirl.create(:store)}
+  
+  def valid_attributes
+    { name: "store_name", 
+      description: "store_description",
+      path: "store_path", 
+      status: "online", 
+      theme: "default" }
+  end
+
+  def valid_session
+    {}
+  end
+
+  describe "GET index" do
+    it "assigns all stores as @stores" do
+      get :index, {}, valid_session
+      assigns(:stores).should eq([store])
+    end
+  end
+
+  describe "GET new" do
+    it "assigns a new store as @store" do
+      get :new, {}, valid_session
+      assigns(:new_store).should be_a_new(Store)
+    end
+  end
+
+  describe "POST create" do
+    describe "with valid params" do
+      it "creates a new Store" do
+        expect {
+          post :create, {:store => valid_attributes}, valid_session
+        }.to change(Store, :count).by(1)
+      end
+
+      it "assigns a newly created store as @store" do
+        post :create, {:store => valid_attributes}, valid_session
+        assigns(:new_store).should be_a(Store)
+        assigns(:new_store).should be_persisted
+      end
+
+      it "redirects to the created store" do
+        post :create, {:store => valid_attributes}, valid_session
+        response.should render_template(:confirmation)
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved store as @store" do
+        Store.any_instance.stub(:save).and_return(false)
+        post :create, {:store => { "name" => "invalid value" }}, valid_session
+        assigns(:new_store).should be_a_new(Store)
+      end
+
+      it "re-renders the 'new' template" do
+        Store.any_instance.stub(:save).and_return(false)
+        post :create, {:store => { "name" => "invalid value" }}, valid_session
+        response.should render_template("new")
+      end
+    end
+  end
+end
