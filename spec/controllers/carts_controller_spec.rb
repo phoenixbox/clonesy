1a8fecb7a5eda73bca41a80f9e52280276bcfe04
Geoff Schorkopf <gschorkopf@gmail.com>
2013-04-23 17:43:07 -0600

    Carts controller complete

---
+++ spec/controllers/carts_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe CartsController do
+  let!(:store){FactoryGirl.create(:store)}
+  let!(:p){FactoryGirl.create(:product, store: store)}
+
+  before(:each) do
+    @cart = Cart.new({})
+    controller.stub(:current_cart => @cart)
+    request.env["HTTP_REFERER"] = '/'
+  end
+
+  describe "Carts#Update" do
+    it "updates the cart details" do
+      expect(@cart.count).to eq nil
+      put :update, carts: {product_id: p.id, quantity: '10'}
+      expect(@cart.count).to eq "(10)"
+    end
+  end
+
+  describe "Carts#Remove_Item" do
+    it "removes the item from the cart" do
+      put :update, carts: {product_id: p.id, quantity: '10'}
+      expect(@cart.empty?).to eq false
+      put :remove_item, product_id: p.id, store_path: store.path
+      expect(@cart.empty?).to eq true
+    end
+  end
+
+  describe "Carts#Destroy" do
+    it "removes all items from cart" do
+      put :update, carts: {product_id: p.id, quantity: '10'}
+      expect(@cart.empty?).to eq false
+      delete :destroy, store_path: store.path
+      expect(@cart.empty?).to eq true
+    end
+  end
+
+end
+
