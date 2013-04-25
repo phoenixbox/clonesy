5e634cc8216621838523e704f5c7714eb1d0a63d
Geoff Schorkopf <gschorkopf@gmail.com>
2013-04-24 21:00:56 -0600

    Finalized some front end work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def show
     @stores = [[Store.first, "Featured Store:"], [Store.last, "Newest Store:"]]
     @products = Product.all.sample(4)
+    @listings = @products + Product.all.sample(2)
   end
 
 end
