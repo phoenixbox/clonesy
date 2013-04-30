9769c0a0cda7c13a3b016ac4129f3e8b93874ac1
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-04-29 18:20:35 -0600

    user registering for the application has a favorites collection created upon registration

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe Collection do
 
   let!(:user){ FactoryGirl.create(:user) }
+  let!(:store) {FactoryGirl.create(:store)}
+  let!(:product) { FactoryGirl.create(:product, store: store) }
   
   it "validates presence of collection name" do
     Collection.create(theme: "a_theme", user_id: user.id)
@@ -19,4 +21,16 @@ describe Collection do
     expect(Collection.count).to eq 0 
   end
 
+  context "given a user has added products to an existing collection" do 
+
+    let!(:collection) { Collection.create(name: "a_name", theme: "a_theme", user_id: user.id) }
+
+    before do 
+      collection.products << product
+    end
+
+    it "generates a random image for the collection for views" do 
+      expect(collection.sample_collection_image).to be_kind_of(Paperclip::Attachment)
+    end
+  end
 end
