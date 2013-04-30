188f85d291f5773bb9a44a276d3ad3688364aca9
Geoff Schorkopf <gschorkopf@gmail.com>
2013-04-30 15:48:50 -0600

    Made a fucking css circle

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -2,9 +2,11 @@ $(function () {
 
   $('.product-display').on('mouseover', function () {
     $(this).find('.product-favorite').show();
+    $(this).find('.circle').show();
   });
   $('.product-display').on('mouseleave', function () {
     $(this).find('.product-favorite').hide();
+    $(this).find('.circle').hide();
   });
 
 
