33d09c96f24f2cef7e692a35201c753f751c5fef
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-01 18:27:47 -0600

    User now added as admin to store upon creation

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -7,6 +7,7 @@ class StoresController < ApplicationController
     @new_store = Store.new(params[:store])
 
     if @new_store.save
+      Role.promote(current_user, @new_store, 'admin')
       render :confirmation
     else
       render :new
